93c5df39ed8b7c7aebe79ca494fcd3d6ee2bb52b
William Tsay <willtsay@WTSAY.local>
2014-05-23 11:32:28 -0700

    just committing for kicks

--- javascript/view.js
+++ javascript/view.js
@@ -1,9 +1,26 @@
-View = function() {
+function makeBoard(){
+  $('.col').each(function(){
+    if (this.getContext){
+      var ctx = this.getContext("2d")
+    for (var i = 1; i <= 7; i++){
+      ctx.beginPath()
+      ctx.arc(50,i*100-50,45,0,2*Math.PI)
+      ctx.lineWidth=6
+      ctx.stroke()
+      ctx.fillStyle="#FFFFFF"
+      ctx.fill()
 
+    }
+    }
+  })      
 }
 
+makeBoard()
 
-View.prototype = {
+function View(){
+}
+
+View.prototype({
   makeBoard: function(){
     $('.col').each(function(){
       if (this.getContext){
@@ -30,7 +47,39 @@ View.prototype = {
     ctx.fillStyle="#FF0000"
     ctx.fill()
   }
+
+})
+
+
+
+
+
+  filling with an image
+  var imageObj = document.createElement('img')
+  imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg'
+
+  imageObj.onload = function(){
+    ctx.save()
+    ctx.beginPath()
+    ctx.arc(50,row*100-50,45,0,2*Math.PI, true)
+    ctx.closePath()
+    ctx.clip()
+
+    ctx.drawImage(imageObj,-175,row*100-130)
+    
+    ctx.beginPath()
+    ctx.arc(50,row*100-50,45,0,2*Math.PI, true)
+    ctx.clip()
+    ctx.closePath()
+    ctx.restore()    
+  }
+
+
+
+
+
 }
-view = new View
-view.makeBoard()
-view.placePiece(1,6)
+
+fillCircle(1,6)
+fillCircle(2,6)
+makeBoard()
