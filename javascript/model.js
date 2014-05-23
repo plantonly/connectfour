4ba46cf43cac11745a23433a4384db4ae85ba7e4
William Tsay <willtsay@WTSAY.local>
2014-05-23 15:27:15 -0700

    win conditions/game freezes after winning

--- javascript/model.js
+++ javascript/model.js
@@ -20,6 +20,26 @@ Column.prototype = {
       index--;
     }
     return -1
+  },
+  fourInCol: function(){
+    var count = 0
+    var previous = 0
+    for (var index = 0; index < 6; index++){
+      if (previous === 0){
+        previous = this.rows[index].player
+      }
+      if (this.rows[index].player == previous && previous != 0){
+        previous = this.rows[index].player
+        count++
+      }
+      else {
+        count = 0
+        previous = this.rows[index].player
+      }
+      if (count == 4){
+        return true
+      }
+    }
   }
 }
 
@@ -31,5 +51,71 @@ ConnectFour = function() {
 ConnectFour.prototype = {
   makeMove: function(player, column) {
     return this.board[column].makeMove(player)
+  },
+  checkCol: function(col){
+    return this.board[col].fourInCol()
+  },
+  checkRow: function(col,row){
+    var count = 0
+    var previous = 0
+    var key = this.getValue(col,row)
+    for (var column = 0; column < 7; column++){
+      if (this.board[column].rows[row].player == key){
+        count++
+      }
+      else {
+        count = 0
+      }
+      if (count == 4){
+        return true
+      }
+    }
+  },
+  getValue: function(col,row){
+    return this.board[col].rows[row].player
+  },
+  checkDiag: function(col, row){
+    var d1 = [this.getValue(0,0),this.getValue(1,1),this.getValue(2,2),this.getValue(3,3),this.getValue(4,4),this.getValue(5,5)]
+    var d2 = [this.getValue(1,0),this.getValue(2,1),this.getValue(3,2),this.getValue(4,3),this.getValue(5,4),this.getValue(6,5)]
+    var d3 = [this.getValue(2,0),this.getValue(3,1),this.getValue(4,2),this.getValue(5,3),this.getValue(6,4)]
+    var d4 = [this.getValue(3,0),this.getValue(4,1),this.getValue(5,2),this.getValue(6,3)]
+    var d5 = [this.getValue(0,1),this.getValue(1,2),this.getValue(2,3),this.getValue(3,4),this.getValue(4,5)]
+    var d6 = [this.getValue(0,2),this.getValue(1,3),this.getValue(2,4),this.getValue(3,5)]
+    var d7 = [this.getValue(0,3),this.getValue(1,2),this.getValue(2,1),this.getValue(3,0)]
+    var d8 = [this.getValue(0,4),this.getValue(1,3),this.getValue(2,2),this.getValue(3,1),this.getValue(4,0)]
+    var d9 = [this.getValue(0,5),this.getValue(1,4),this.getValue(2,3),this.getValue(3,2),this.getValue(4,1),this.getValue(5,0)]
+    var d10 = [this.getValue(1,5),this.getValue(2,4),this.getValue(3,3),this.getValue(4,2),this.getValue(5,1),this.getValue(6,0)]
+    var d11 = [this.getValue(2,5),this.getValue(3,4),this.getValue(4,3),this.getValue(5,2),this.getValue(6,1)]
+    var d12 = [this.getValue(3,5),this.getValue(4,4),this.getValue(5,3),this.getValue(6,2)]
+    var all = [d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12]
+    var key = this.board[col].rows[row].player
+    for (var diag = 0; diag< 12; diag++){
+      loopAmount = all[diag].length
+      var previous = 0
+      var count = 0
+      for (var index = 0; index < loopAmount; index++ ){
+        if (all[diag][index] == key){
+          count++
+        }
+        else {
+          count = 0
+        }
+        if (count == 4){
+          return true
+        }
+      }
+    }
+  },
+  checkWin: function(col, row){
+    if ( this.checkCol(col) || this.checkRow(col, row) || this.checkDiag(col,row) ) {
+      alert("WIN")
+      return true
+    }
   }
 }
+
+
+
+
+
+
--- javascript/view.js
+++ javascript/view.js
@@ -1,22 +1,3 @@
-function makeBoard(){
-  $('.col').each(function(){
-    if (this.getContext){
-      var ctx = this.getContext("2d")
-    for (var i = 1; i <= 7; i++){
-      ctx.beginPath()
-      ctx.arc(50,i*100-50,45,0,2*Math.PI)
-      ctx.lineWidth=6
-      ctx.stroke()
-      ctx.fillStyle="#FFFFFF"
-      ctx.fill()
-
-    }
-    }
-  })      
-}
-
-makeBoard()
-
 function View(){
 }
 
@@ -55,34 +36,3 @@ View.prototype({
   }
 
 })
-
-
-
-
-
-  filling with an image
-  var imageObj = document.createElement('img')
-  imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg'
-
-  imageObj.onload = function(){
-    ctx.save()
-    ctx.beginPath()
-    ctx.arc(50,row*100-50,45,0,2*Math.PI, true)
-    ctx.closePath()
-    ctx.clip()
-
-    ctx.drawImage(imageObj,-175,row*100-130)
-    
-    ctx.beginPath()
-    ctx.arc(50,row*100-50,45,0,2*Math.PI, true)
-    ctx.clip()
-    ctx.closePath()
-    ctx.restore()    
-  }
-
-
-
-
-
-}
-
