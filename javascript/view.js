d904a84c47b2027a41e96fcca5640fca7cf9f38f
William Tsay <willtsay@WTSAY.local>
2014-05-22 18:02:37 -0700

    view is now in mvc ish format

--- javascript/view.js
+++ javascript/view.js
@@ -1,59 +1,36 @@
-function makeBoard(){
-  $('.col').each(function(){
-    if (this.getContext){
-      var ctx = this.getContext("2d")
-    for (var i = 1; i <= 7; i++){
-      ctx.beginPath()
-      ctx.arc(50,i*100-50,45,0,2*Math.PI)
-      ctx.lineWidth=6
-      ctx.stroke()
-      ctx.fillStyle="#FFFFFF"
-      ctx.fill()
+View = function() {
 
-    }
-    }
-  })      
 }
 
-makeBoard()
 
-function fillCircle(id,row){
-  var col = document.getElementById(id)
-  var ctx = col.getContext("2d")
-  // ctx.beginPath()
-  // ctx.arc(50,row*100-50,45,0,2*Math.PI)
-  // ctx.lineWidth=6
-  // ctx.stroke()
-  // var imageObj = new Image()
-
-  // ctx.fillStyle="#FF0000"
-  // ctx.fill()
-
-  var imageObj = document.createElement('img')
-  imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg'
-
-  imageObj.onload = function(){
-    ctx.save()
+View.prototype = {
+  makeBoard: function(){
+    $('.col').each(function(){
+      if (this.getContext){
+        var ctx = this.getContext("2d")
+        for (var i = 1; i <= 7; i++){
+          ctx.beginPath()
+          ctx.arc(50,i*100-50,45,0,2*Math.PI)
+          ctx.lineWidth=6
+          ctx.stroke()
+          ctx.fillStyle="#FFFFFF"
+          ctx.fill()
+        }
+      }
+    })      
+  },
+  placePiece: function (id,row){
+    var col = document.getElementById(id)
+    var ctx = col.getContext("2d")
     ctx.beginPath()
-    ctx.arc(50,row*100-50,45,0,2*Math.PI, true)
-    ctx.closePath()
-    ctx.clip()
-
-    ctx.drawImage(imageObj,-175,row*100-130)
-    
-    ctx.beginPath()
-    ctx.arc(50,row*100-50,45,0,2*Math.PI, true)
-    ctx.clip()
-    ctx.closePath()
-    ctx.restore()    
-
+    ctx.arc(50,row*100-50,45,0,2*Math.PI)
+    ctx.lineWidth=6
+    ctx.stroke()
+    var imageObj = new Image()
+    ctx.fillStyle="#FF0000"
+    ctx.fill()
   }
-
-
-
-
-
 }
-
-fillCircle(1,6)
-fillCircle(2,6)
+view = new View
+view.makeBoard()
+view.placePiece(1,6)
