ef2246ca802e19317e01ec2560596945f6b61563
Tiff & Ali <pair+Tkais.alistairhill@devbootcamp.com>
2014-05-22 19:43:27 -0700

    Completed Stories - Users can place a piece in a column - Users can see a board - Users can place alternating black and red pieces on the board

--- javascript/view.js
+++ javascript/view.js
@@ -8,7 +8,7 @@ View.prototype = {
     $('.col').each(function(){
       if (this.getContext){
         var ctx = this.getContext("2d")
-        for (var i = 1; i <= 7; i++){
+        for (var i = 1; i <= 6; i++){
           ctx.beginPath()
           ctx.arc(50,i*100-50,45,0,2*Math.PI)
           ctx.lineWidth=6
@@ -17,20 +17,24 @@ View.prototype = {
           ctx.fill()
         }
       }
-    })      
+    })
   },
-  placePiece: function (id,row){
+  placePiece: function (id,row,player){
     var col = document.getElementById(id)
     var ctx = col.getContext("2d")
+    row++
     ctx.beginPath()
     ctx.arc(50,row*100-50,45,0,2*Math.PI)
     ctx.lineWidth=6
     ctx.stroke()
     var imageObj = new Image()
+    if (player == 1) {
     ctx.fillStyle="#FF0000"
+    }
+    else {
+    ctx.fillStyle="#000000"
+    }
     ctx.fill()
   }
 }
-view = new View
-view.makeBoard()
-view.placePiece(1,6)
+
